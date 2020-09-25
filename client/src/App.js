177c8d24c629d0212df9a4f69633276013aefb42
Yuvraj Singh <yuvraj.singh2018@vitstudent.ac.in>
2020-09-25 19:17:45 +0530

    Integrated frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+	const [question, setQuestion] = useState('');
+	const [tags, setTags] = useState([]);
+
+	const fetchTags = async() => {
+		console.log(question)
+		let response = await fetch('/api/getRecommendedTags', {
+			headers:{
+				'content-type': 'application/json'
+			},
+			method: 'post',
+			body: JSON.stringify({
+				question
+			})
+		})
+
+		response = await response.json();
+		console.log(response)
+
+		// SET TAGS
+		setTags(response)
+
+	}
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src="https://cdn.sstatic.net/Sites/stackoverflow/company/Img/logos/so/so-logo.svg?v=a010291124bf" className="stackoverflow_logo" alt="logo"/>
+			</header>
+
+			<p id="header__text">Enter the question you intend to post to Stackoverflow to <span>generate a list of apt tags for the question!</span></p>
+
+			<textarea 
+				placeholder="Enter question here..."
+				className="question__input"
+				onChange={e => setQuestion(e.target.value)}
+			/>
+			<br />
+			<button 
+				id="submit"
+				onClick={fetchTags}
+			>
+				Get Tags!
+			</button>
+
+			{/* PREDICTED TAGS */}
+			
+			<div className="stackoverflow__tag__container">
+			{
+				tags.map((tag) => {
+					return <PredictedTag 
+					key= {tag}
+					tag={tag}
+				/>
+				})
+			}
+			</div>
+			
+		</div>
+	);
 }
 
-export default App;
+// PREDICTED TAG COMPONENT
+const PredictedTag = ({tag}) => {
+	return(
+		<div className="stackoverflow__tag">
+			{tag}
+		</div>
+	)
+}
